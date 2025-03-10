c14c44644163b8bd6c1a231221359a7a1d0e19e7
Sovanda <149866068+Sovandaa@users.noreply.github.com>
2025-03-10 17:33:58 +0700

    update key enviroment

--- api/controller/lib/cloud_function/index.js
+++ api/controller/lib/cloud_function/index.js
@@ -1,29 +1,37 @@
-// require('dotenv').config();
+require('dotenv').config();
 const admin = require('firebase-admin');
 
-const serviceAccount = require("../../../config/serviceAccountKey.json");  // get account key to access to firebase project
+// const serviceAccount = require("../../../config/serviceAccountKey.json");  // get account key to access to firebase project
 
-// /// Testing to access firebase sdk
-// const serviceAccount = {
-//   type: process.env.SERVICE_ACCOUNT_TYPE,
-//   project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
-//   private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-//   private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//   client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
-//   client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
-//   auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
-//   token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
-//   client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
-// };
+// Convert the PRIVATE_KEY properly (because \n must be replaced with actual newlines)
+const serviceAccount = {
+  type: process.env.SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.SERVICE_ACCOUNT_PROJECT_ID,
+  private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+  private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix newlines
+  client_email: process.env.SERVICE_ACCOUNT_CLIENT_EMAIL,
+  client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
+  auth_uri: process.env.SERVICE_ACCOUNT_AUTH_URI,
+  token_uri: process.env.SERVICE_ACCOUNT_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.SERVICE_ACCOUNT_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.SERVICE_ACCOUNT_UNIVERSE_DOMAIN
+};
 
-
-// debuging the specific json file path
-console.log('Resolved path:', serviceAccount);
-
+// Initialize Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
+
+console.log('Firebase initialized successfully!');
+
+
+// // // debuging the specific json file path
+// // console.log('Resolved path:', serviceAccount);
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 const db = admin.firestore();
 
@@ -97,9 +105,6 @@ async function checkTenantsRegistered(systemId, chatId) {
     console.error('Error checking notification:', error);
   }
 }
-
-
-
 
 
 module.exports = { storeNotification, checkTenantsRegistered }
