d5c132f9d85f746c0ac90bcc54357087950cecb6
Sovanda <149866068+Sovandaa@users.noreply.github.com>
2025-03-14 13:20:38 +0700

    update check register

--- api/controller/lib/telegram/registration.js
+++ api/controller/lib/telegram/registration.js
@@ -6,145 +6,182 @@ const { storeNotification } = require("../cloud_function/index");
 
 const registrationSteps = {}; // track user registration steps
 
-
 function formatValidName(value, helpers) {
-    let validName = value
-                .trim()
-                .replace(/\s+/g, " ") // Replace multiple spaces with a single space
-                .replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize first letter of each word
+  let validName = value
+    .trim()
+    .replace(/\s+/g, " ") // Replace multiple spaces with a single space
+    .replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize first letter of each word
 
-    if (!validName) return helpers.error("any.invalid");
+  if (!validName) return helpers.error("any.invalid");
 
-    // Corrected regex pattern to allow full names with spaces
-    const namePattern = /^[a-zA-Z\u00C0-\u00FF' ]+$/;
-    if (!namePattern.test(validName)) return helpers.error("any.invalid");
+  // Corrected regex pattern to allow full names with spaces
+  const namePattern = /^[a-zA-Z\u00C0-\u00FF' ]+$/;
+  if (!namePattern.test(validName)) return helpers.error("any.invalid");
 
-    // Remove unnecessary words from the name
-    const nameIndicators = ["name", "is", "my", "i'm", "am", "called", "call", "full", "first", "last", "middle"];
-    let nameParts = validName.split(/\s+/).map(part => part.replace(/'s$/, ""));
-    let filteredName = nameParts.filter(part => !nameIndicators.includes(part.toLowerCase())).join(" ");
+  // Remove unnecessary words from the name
+  const nameIndicators = [
+    "name",
+    "is",
+    "my",
+    "i'm",
+    "am",
+    "called",
+    "call",
+    "full",
+    "first",
+    "last",
+    "middle",
+  ];
+  let nameParts = validName.split(/\s+/).map((part) => part.replace(/'s$/, ""));
+  let filteredName = nameParts
+    .filter((part) => !nameIndicators.includes(part.toLowerCase()))
+    .join(" ");
 
-    if (!filteredName.trim()) return helpers.error("any.invalid");
+  if (!filteredName.trim()) return helpers.error("any.invalid");
 
-    return filteredName;  
+  return filteredName;
 }
 
 const registrationSchema = Joi.object({
-    name: Joi.string()
-        .trim()
-        .max(30)
-        .custom(formatValidName)
-        .required()
-        .messages({
-            "any.invalid": "Name must contain only letters and spaces.",
-        }),
+  name: Joi.string()
+    .trim()
+    .max(30)
+    .custom(formatValidName)
+    .required()
+    .messages({
+      "any.invalid": "Name must contain only letters and spaces.",
+    }),
 
-    phone: Joi.string()
-        .trim()
-        .pattern(/^\+?\d{9,15}$/) // Allows an optional '+' followed by 9-15 digits
-        .required()
-        .messages({
-            "string.pattern.base": "Phone number must be a 9/10 digits number.",
-        }),
-    
-    idIdentification: Joi.string()
-        .trim()
-        .pattern(/^\d{10,12}$/)
-        .required()
-        .messages({
-            "string.pattern.base": "ID card number must be a 10/12 digits number.",
-        }),
+  phone: Joi.string()
+    .trim()
+    .pattern(/^\+?\d{9,15}$/) // Allows an optional '+' followed by 9-15 digits
+    .required()
+    .messages({
+      "string.pattern.base": "Phone number must be a 9/10 digits number.",
+    }),
 
+  idIdentification: Joi.string()
+    .trim()
+    .pattern(/^\d{10,12}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "ID card number must be a 10/12 digits number.",
+    }),
 });
 
 async function handleRegistration(messageObj) {
-    const chatId = messageObj.chat.id;
-    const msgText = messageObj.text;
-    const step = registrationSteps[chatId]?.step; 
-    let systemId = "MF3DBs9vbee9yw0jwfBjK9kIGXs2";  // sample systemId for testing
-    let dataType = "registration";
-    let status = "pending"; 
+  const chatId = messageObj.chat.id;
+  const msgText = messageObj.text;
+  const step = registrationSteps[chatId]?.step;
+  let systemId = "MF3DBs9vbee9yw0jwfBjK9kIGXs2"; // sample systemId for testing
+  let dataType = "registration";
+  let status = "pending";
 
-    if (!registrationSteps[chatId]) {
-            registrationSteps[chatId] = { 
-                step: 1, 
-                chat_id: messageObj.chat.id 
-            };
-            return sendMessage(messageObj, "Enter your full name:\nExample: John Doe");
-    }
+  if (!registrationSteps[chatId]) {
+    registrationSteps[chatId] = {
+      step: 1,
+      chat_id: messageObj.chat.id,
+    };
+    return sendMessage(messageObj, "Enter your full name:\nExample: John Doe");
+  }
 
-    switch (step) {
-        case 1:
-            // Validate name
-            const { error: nameErr, value: validName } = Joi.object({ name: registrationSchema.extract("name") })
-            .validate({ name: msgText });
-        
-            if (nameErr) return sendMessage(messageObj, "Invalid Name, pls re-enter:\nExample: Jonh Doe");
-            
-            registrationSteps[chatId].name = validName.name; // Store cleaned-up name
-            registrationSteps[chatId].step = 2;
-            return sendMessage(messageObj, "Enter your phone number:\nExample: 012345678 or +85512345678");
+  switch (step) {
+    case 1:
+      // Validate name
+      const { error: nameErr, value: validName } = Joi.object({
+        name: registrationSchema.extract("name"),
+      }).validate({ name: msgText });
 
-        case 2:
-            // Validate phone number
-            const { error: phoneErr, value: validPhone } = Joi.object({ phone: registrationSchema.extract("phone")}).validate({ phone: msgText });
-            if (phoneErr) return sendMessage(messageObj, "Invalid Phone number, pls re-enter:\nExample: 012345678 or +85512345678");
+      if (nameErr)
+        return sendMessage(
+          messageObj,
+          "Invalid Name, pls re-enter:\nExample: Jonh Doe"
+        );
 
-            registrationSteps[chatId].phone = validPhone.phone;
-            registrationSteps[chatId].step = 3;
-            return sendMessage(messageObj, "Enter your ID Identification number:\nExample: 1234567890");
+      registrationSteps[chatId].name = validName.name; // Store cleaned-up name
+      registrationSteps[chatId].step = 2;
+      return sendMessage(
+        messageObj,
+        "Enter your phone number:\nExample: 012345678 or +85512345678"
+      );
 
-        case 3:
-            // Validate ID card number
-            const { error: idIdentifyErr, value: validIdentify } = Joi.object({ idIdentification: registrationSchema.extract("idIdentification")}).validate({ idIdentification: msgText });
-            if (idIdentifyErr) return sendMessage(messageObj, "Invalid ID Identify number, pls re-enter:\nExample: 1234567890");
+    case 2:
+      // Validate phone number
+      const { error: phoneErr, value: validPhone } = Joi.object({
+        phone: registrationSchema.extract("phone"),
+      }).validate({ phone: msgText });
+      if (phoneErr)
+        return sendMessage(
+          messageObj,
+          "Invalid Phone number, pls re-enter:\nExample: 012345678 or +85512345678"
+        );
 
-            registrationSteps[chatId].id_Identification = validIdentify.idIdentification;
-            registrationSteps[chatId].registration_date = new Date().toISOString();
+      registrationSteps[chatId].phone = validPhone.phone;
+      registrationSteps[chatId].step = 3;
+      return sendMessage(
+        messageObj,
+        "Enter your ID Identification number:\nExample: 1234567890"
+      );
 
-            // Prepare the data registration into JSON forma after all steps are complete
-            const tenantDataToStore = {
-                // id: uuidv4(),
-                systemID: systemId,
-                chatID: chatId.toString(),
-                dataType: dataType,
-                read: false,
-                status: status,
-                isApprove: false,
-                notifyData:{
-                    name: registrationSteps[chatId].name.toString(),
-                    phone: registrationSteps[chatId].phone.toString(),
-                    idIdentification: registrationSteps[chatId].id_Identification.toString(),
-                    registerOnDate: registrationSteps[chatId].registration_date,
-                }
+    case 3:
+      // Validate ID card number
+      const { error: idIdentifyErr, value: validIdentify } = Joi.object({
+        idIdentification: registrationSchema.extract("idIdentification"),
+      }).validate({ idIdentification: msgText });
+      if (idIdentifyErr)
+        return sendMessage(
+          messageObj,
+          "Invalid ID Identify number, pls re-enter:\nExample: 1234567890"
+        );
 
-            };
+      registrationSteps[chatId].id_Identification =
+        validIdentify.idIdentification;
+      registrationSteps[chatId].registration_date = new Date().toISOString();
 
-            console.log("Received Registration data:", JSON.stringify(tenantDataToStore, null, 2));
+      // Prepare the data registration into JSON forma after all steps are complete
+      const tenantDataToStore = {
+        // id: uuidv4(),
+        systemID: systemId,
+        chatID: chatId.toString(),
+        dataType: dataType,
+        read: false,
+        status: status,
+        isApprove: false,
+        notifyData: {
+          name: registrationSteps[chatId].name.toString(),
+          phone: registrationSteps[chatId].phone.toString(),
+          idIdentification: registrationSteps[chatId].id_Identification.toString(),
+          registerOnDate: registrationSteps[chatId].registration_date,
+        },
+      };
 
-            //show info registered to tenant
-            const showInfo = "";
+      // Prepare tenant information message
+      const showInfo = `📌 Here are your registration details:\n\n• Name: ${registrationSteps[chatId].name}\n• Phone: ${registrationSteps[chatId].phone}\n• ID: ${registrationSteps[chatId].id_Identification}\n• Registered On: ${registrationSteps[chatId].registration_date}\n⏳ Please wait for the landlord to approve your registration.\n\n You will be notified once your registration is approved. Thank you! 😊`;
 
-            // Save tenant data to file, just testing in local
-            // saveTenantsRegistration(tenantDataToStore);
+      console.log(
+        "Received Registration data:",
+        JSON.stringify(tenantDataToStore, null, 2)
+      );
 
-            // store to firebase use cloud function 
-            storeNotification(systemId, tenantDataToStore);
+      // Save tenant data to file, just testing in local
+      // saveTenantsRegistration(tenantDataToStore);
 
-            delete registrationSteps[chatId]; // Registration complete
+      // store to firebase use cloud function
+      storeNotification(systemId, tenantDataToStore);
 
-            return sendMessage(
-                messageObj,
-                "Please waiting for landlord to approve your registration!",
-            );
+      delete registrationSteps[chatId]; // Registration complete
 
-        default:
-            return sendMessage(messageObj, "Sorry, I don't understand.");
-    }
+      return sendMessage(
+        messageObj,
+        "Please waiting for landlord to approve your registration!"
+      );
+
+    default:
+      return sendMessage(messageObj, "Sorry, I don't understand.");
+  }
 }
 
-module.exports = { 
-    registrationSteps,
-    handleRegistration
- };
-
+module.exports = {
+  registrationSteps,
+  handleRegistration,
+};
