38edcd8594a8dabcf65a6307f6d08a77dbf7fad9
Sovanda <149866068+Sovandaa@users.noreply.github.com>
2025-03-18 16:17:49 +0700

    modify handleKhqrStatus

--- api/controller/lib/khqr/khqr.js
+++ api/controller/lib/khqr/khqr.js
@@ -92,7 +92,7 @@ async function handleKHQRstatus(md5,res) {
             
           });
         console.log("res successfully");
-        res.json({data: response.data});
+        res.json(response.data);
         console.log(`Respone Data: ${JSON.stringify(response.data, null ,2)}`);
 
       } catch (error) {
